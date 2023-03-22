faad59832334520c68fe60a3fbf20bee1aa7c834
BRAJESWAR GHOSH <brajeswar520@gmail.com>
2023-03-22 13:57:47 +0530

    testimonial

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from "./Product";
 import { productData, responsive } from "./Utilis/Data";
 
 export default function Testimonials(props) {
 
+    const [productCount, setProductCount] = useState(0);
+
     const btnpressprev = () => {
-        const box = document.getElementById("product-container");
-        let width = box.clientWidth;
-        box.scrollLeft = box.scrollLeft - width;
+        const ele = document.getElementById("product-container");
+        let width = ele.clientWidth;
+        ele.scrollLeft = ele.scrollLeft - width;
         console.log(width)
+        setProductCount(productCount - 1);
     };
 
     const btnpressnext = () => {
-        const box = document.getElementById("product-container");
-        let width = box.clientWidth;
-        box.scrollLeft = box.scrollLeft + width;
+        const ele = document.getElementById("product-container");
+        let width = ele.clientWidth;
+        ele.scrollLeft = ele.scrollLeft + width;
         console.log(width)
+        setProductCount(productCount + 1);
     }
 
     const product = productData.map((item) => (
@@ -38,9 +42,9 @@ export default function Testimonials(props) {
                 &nbsp;&nbsp;
             </div>
             <div className="product-carousel">
-                <button className="pre-btn" onClick={btnpressprev}><p>&lt;</p></button>
-                <button className="next-btn" onClick={btnpressnext}><p>&gt;</p></button>
-
+                <button className="pre-btn" id='pre-btn' onClick={btnpressprev} disabled={productCount <= 0} ><p>&lt;</p></button>
+                <button className="next-btn" onClick={btnpressnext} disabled={productCount >= 3} ><p>&gt;</p></button>
+                
                 <div className="product-container" id="product-container" responsive={responsive} >
                     {product}
                 </div>
